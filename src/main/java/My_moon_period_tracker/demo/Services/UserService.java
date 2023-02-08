adc5faa7321a58f6444d2cbb6af863d8f4b960ca
rimmderes <rimmderes@gmail.com>
2023-02-08 11:26:58 +0000

    started userController

--- src/main/java/My_moon_period_tracker/demo/Services/UserService.java
+++ src/main/java/My_moon_period_tracker/demo/Services/UserService.java
@@ -5,11 +5,19 @@ import My_moon_period_tracker.demo.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
     @Autowired
     UserRepository userRepository;
+
+    // get all users
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
 
     // get user by id
 
