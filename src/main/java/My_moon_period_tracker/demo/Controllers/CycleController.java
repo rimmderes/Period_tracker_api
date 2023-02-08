7753ff71c0ad512411e985a1acfef1284fb0efba
rimmderes <rimmderes@gmail.com>
2023-02-08 14:35:37 +0000

    added get and patch requests in cycle controller

--- src/main/java/My_moon_period_tracker/demo/Controllers/CycleController.java
+++ src/main/java/My_moon_period_tracker/demo/Controllers/CycleController.java
@@ -4,9 +4,14 @@ import My_moon_period_tracker.demo.Models.Cycle;
 import My_moon_period_tracker.demo.Repositories.CycleRepository;
 import My_moon_period_tracker.demo.Services.CycleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/cycle")
@@ -29,11 +34,26 @@ public class CycleController {
     // update a cycle
     @PatchMapping(value = "/{id}")
     public ResponseEntity<Cycle> updateCycle(@RequestBody Cycle cycle, @PathVariable Long id) {
-        Cycle cycleToUpdate = cycleService.getCycleById(id)
+        cycleService.updateCycle(cycle, id);
+        Optional <Cycle> updatedCycle = cycleService.getCycleById(id);
+        return new ResponseEntity<>(updatedCycle.get(), HttpStatus.OK);
     }
 
 
     // find by start date
+
+    @GetMapping
+    public ResponseEntity<List<Cycle>> getCyclebyStartDate (
+            @RequestParam(required=false, name="startdate") @DateTimeFormat(iso=DateTimeFormat.ISO.DATE)
+            LocalDate startDate) {
+        if(startDate != null) {
+            return new ResponseEntity<>(cycleService.findByStartDate(startDate), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(cycleService.getAllCycles(), HttpStatus.OK);
+    }
+
+
+    // find by last date
 
 //    @GetMapping
 //    public ResponseEntity<>
