2252dc4ddfb3c382dcfff956a6239220ef58096b
Khalil <khalil.hersi@hotmail.co.uk>
2023-02-08 09:45:51 +0000

    adds Cycle model

--- src/main/java/My_moon_period_tracker/demo/Models/Cycle.java
+++ src/main/java/My_moon_period_tracker/demo/Models/Cycle.java
@@ -1,4 +1,44 @@
 package My_moon_period_tracker.demo.Models;
 
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Comment;
+import org.springframework.core.SpringVersion;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity(name = "cycles")
 public class Cycle {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "lastDate")
+    private LocalDate lastDate;
+
+    @Column (name = "startDate")
+    private LocalDate startDate;
+
+    @Column (name = "lateDays")
+    private int lateDays;
+
+    @Column  (name = "emotions")
+    private Enum emotions;
+
+    @Column  (name = "symptoms")
+    private Enum symptoms;
+
+    @Column  (name = "flow")
+    private Enum flow;
+
+    @ManyToOne
+    private User user;
+
+//    public Cycle(long id, LocalDate lastDate, LocalDate startDate, LocalDate lateDays Enum emotions, Enum symptoms, Enum flow){
+//        this.id
+//    }
+
 }
