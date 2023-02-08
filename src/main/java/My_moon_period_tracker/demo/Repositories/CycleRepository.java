b7d1ce64f59b51bb1cf41e9a18d206cfcd5e931c
rimmderes <rimmderes@gmail.com>
2023-02-08 11:22:01 +0000

    added repositories and completed user service

--- src/main/java/My_moon_period_tracker/demo/Repositories/CycleRepository.java
+++ src/main/java/My_moon_period_tracker/demo/Repositories/CycleRepository.java
@@ -1,4 +1,9 @@
 package My_moon_period_tracker.demo.Repositories;
 
-public class CycleRepository {
+import My_moon_period_tracker.demo.Models.Cycle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CycleRepository extends JpaRepository<Cycle, Long> {
 }
--- src/main/java/My_moon_period_tracker/demo/Services/UserService.java
+++ src/main/java/My_moon_period_tracker/demo/Services/UserService.java
@@ -1,4 +1,34 @@
 package My_moon_period_tracker.demo.Services;
 
+import My_moon_period_tracker.demo.Models.User;
+import My_moon_period_tracker.demo.Repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    // get user by id
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+    // add new user
+    public User addNewUser (User user) {
+        userRepository.save(user);
+        return user;
+    }
+
+    // delete user
+    public void deleteUser(long id) {
+        userRepository.deleteById(id);
+    }
+
+
+
+
 }
