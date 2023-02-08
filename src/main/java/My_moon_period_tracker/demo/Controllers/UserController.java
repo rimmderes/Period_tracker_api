97a01d82a595a7934df18f5854adf9c30787e90e
rimmderes <rimmderes@gmail.com>
2023-02-08 11:23:18 +0000

    started user controller

--- src/main/java/My_moon_period_tracker/demo/Controllers/UserController.java
+++ src/main/java/My_moon_period_tracker/demo/Controllers/UserController.java
@@ -1,4 +1,12 @@
 package My_moon_period_tracker.demo.Controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+
+
 }
