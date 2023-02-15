5c7e540c4cf62bb3cb7648934fe05a6f20d639f5
Georgia Clarke <georgiaclarke@Georgias-MacBook-Pro.local>
2023-02-15 15:36:29 +0000

    fixed cors issues

--- src/main/java/My_moon_period_tracker/demo/Configurations/SpringGlobalConfig.java
+++ src/main/java/My_moon_period_tracker/demo/Configurations/SpringGlobalConfig.java
@@ -6,14 +6,15 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
-
 public class SpringGlobalConfig implements WebMvcConfigurer {
 
 
+    @Override
     public void addCorsMappings (CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowedHeaders("*")
+                .allowCredentials(true)
                 .allowedMethods("*");
     }
 }
